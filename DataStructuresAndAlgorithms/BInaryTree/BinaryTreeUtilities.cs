7165bcf2b2c5e02794c159618c1c89a44b965ccb
Ibrahim Swaiss <Iaswaiss@outlook.com>
2020-01-01 19:40:46 +0200

    Add project files.

---
+++ DataStructuresAndAlgorithms/BInaryTree/BinaryTreeUtilities.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+
+namespace DataStructuresAndAlgorithms.BInaryTree {
+    public static class BinaryTreeUtilities {
+        public static void Insert(this BinaryTree root, int value) {
+            if (root == null) {
+                _ = new BinaryTree(value);
+                return;
+            } else {
+                var queue = new Queue<BinaryTree>();
+                queue.Enqueue(root);
+                while (queue.Any()) {
+                    var node = queue.Dequeue();
+
+                    if (node.Left == null) {
+                        node.Left = new BinaryTree(value);
+                        return;
+                    }
+
+                    if (node.Right == null) {
+                        node.Right = new BinaryTree(value);
+                        return;
+                    }
+
+                    queue.Enqueue(node.Left);
+                    queue.Enqueue(node.Right);
+                }
+            }
+        }
+
+        public static void PrintPreOrder(this BinaryTree tree) {
+            
+        }
+        public static void PrintInOrder(this BinaryTree tree) {
+            
+        }
+        public static void PrintPostOrder(this BinaryTree tree) {
+            
+        }
+        public static void PrintLevelOrderTraversal(this BinaryTree tree) {
+            ForegroundColor = System.ConsoleColor.Yellow;
+            if (tree == null) {
+                WriteLine("Empty tree");
+            } else {
+                var queue = new Queue<BinaryTree>();
+                queue.Enqueue(tree);
+                while (queue.Any()) {
+                    var node = queue.Peek();
+
+                    if (node.Left != null)
+                        queue.Enqueue(node.Left);
+                    if (node.Right != null)
+                        queue.Enqueue(node.Right);
+
+                    WriteLine(node.Value);
+                    queue.Dequeue();
+                }
+            }
+            ResetColor();
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,29 @@
+﻿using DataStructuresAndAlgorithms.BInaryTree;
+using static System.Console;
+
+namespace DataStructuresAndAlgorithms {
+    internal class Program {
+        private static void Main(string[] args) {
+            CreateBinaryTree();
+        }
+
+        private static void CreateBinaryTree() {
+            while (true) {
+                WriteLine("Please enter tree size:");
+                int noOfItems = int.Parse(ReadLine());
+                Clear();
+                if (noOfItems < 0) break;
+
+                BinaryTree tree = null;
+                for (int i = 1; i <= noOfItems; i++) {
+                    if (tree == null)
+                        tree = new BinaryTree(i);
+                    else
+                        tree.Insert(i);
+                }
+
+                tree.PrintLevelOrderTraversal(); 
+            }
+        }
+    }
+}
